eccdfd11f524836b36eb7df08e2161789c755443
Chai Phonbopit <phonbopit@gmail.com>
2022-04-21 22:17:16 +0700

    initial

---
+++ README.md
@@ -0,0 +1,6 @@
+Near Contract with AS
+---
+
+Refer to : [สร้าง Smart Contract บน Near Protocol ด้วย Assembly Script](https://devahoy.com/blog/near-example-guestbook-as)
+
+> Note! : Don't forget to change `accountId` in deploy script.
---
+++ assembly/main.ts
@@ -0,0 +1,25 @@
+import { PostedMessage, messages } from "./model";
+
+// The maximum number of latest messages the contract returns.
+const MESSAGE_LIMIT = 10;
+
+export function addMessage(text: string): void {
+  // Creating a new message and populating fields with our data
+  const message = new PostedMessage(text);
+  // Adding the message to end of the the persistent collection
+  messages.push(message);
+}
+
+/**
+ * Returns an array of last N messages.\
+ * NOTE: This is a view method. Which means it should NOT modify the state.
+ */
+export function getMessages(): PostedMessage[] {
+  const numMessages = min(MESSAGE_LIMIT, messages.length);
+  const startIndex = messages.length - numMessages;
+  const result = new Array<PostedMessage>(numMessages);
+  for (let i = 0; i < numMessages; i++) {
+    result[i] = messages[i + startIndex];
+  }
+  return result;
+}
